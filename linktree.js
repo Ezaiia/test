8cf08c69d1a537d1c2817871aefc5846ec3ec868
ahmad <itxti909@gmail.com>
2023-08-23 15:11:25 +0300

    dunno

---
+++ linktree.js
@@ -0,0 +1,16 @@
+document.getElementById("officeAddress-text").innerHTML = `Head Office Address: Camişerif Mah.5248 Sk No:2 JBY PLAZA<br> <p>
+Kat:5 D:26. Akdeniz/ Mersin (İçel),Türkiye. </p>`;
+
+document.getElementById("head-tel-text").innerHTML = ' +90 324 504 04 94';
+
+document.getElementById("wareAddress-text").innerHTML = ` Warehouse Address: Çilek Mah. 63147 Sk. No:1 İç Kapi No:8<br>
+<p>Mega Center Iş Merkezi. Akdeniz/Mersin (İçel),Türkiye. </p> `;
+
+document.getElementById("Ware-tel-text").innerHTML = ' +90 324 504 24 24 ';
+
+document.getElementById("Ware-gsm-text").innerHTML = ' +90 501 373 06 33 ';
+
+document.getElementById("logistics-Gsm-text").innerHTML = " +90 501 121 06 33 ";
+
+document.getElementById("E-mail-text").innerHTML = " info@sialtr.com ";
+
